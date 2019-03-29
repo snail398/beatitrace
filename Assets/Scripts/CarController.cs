d1a6a3777d5bc59f4d75e4e8c776b54ef0f70cc4
snail398 <lololo398@yandex.ru>
2019-03-29 10:02:26 +0300

    main menu, new car, rb move

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
-    [SerializeField] private float maxSpeed = 1f;
-    [SerializeField] private float acceleration = 1f;
-    [SerializeField] private float brake = 1f;
-    [SerializeField] private float control = 5f;
+    [SerializeField] private float maxSpeed_km_per_h = 1f;
+    [SerializeField] private float acceleration_km_per_h2 = 1f;
+    [SerializeField] private float brake_km_per_h2 = 1f;
+    [SerializeField] private float control_degree = 5f;
     [SerializeField] private TimerController timer;
 
     [SerializeField] private Button left;
@@ -20,30 +20,37 @@ public class CarController : MonoBehaviour
     private bool braking = false;
     private float currentSpeed = 0;
     private float currentControl;
-    
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
-        if (currentSpeed <= maxSpeed && !braking)
+        if (currentSpeed <= maxSpeed_km_per_h && !braking)
         {
-            currentSpeed += Time.deltaTime*acceleration;
+            currentSpeed += Time.deltaTime* acceleration_km_per_h2;
         }
-        if (currentSpeed>maxSpeed)
+        if (currentSpeed> maxSpeed_km_per_h)
         {
-            currentSpeed = maxSpeed;
+            currentSpeed = maxSpeed_km_per_h;
         }
-        transform.position += transform.right * currentSpeed;
+
+        rb.MovePosition(transform.position + transform.right * currentSpeed/100); 
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(transform.up, control);
+            transform.Rotate(transform.up, control_degree);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(transform.up, -control);
+            transform.Rotate(transform.up, -control_degree);
         }
-        if (Input.GetKey(KeyCode.S)&&currentSpeed>maxSpeed*0.4f)
+        if (Input.GetKey(KeyCode.S)&&currentSpeed> maxSpeed_km_per_h * 0.4f)
         {
-            currentSpeed -= Time.deltaTime * brake;
+            currentSpeed -= Time.deltaTime * brake_km_per_h2;
             braking = true;
         }
         if (Input.GetKeyUp(KeyCode.S))
@@ -51,9 +58,9 @@ public class CarController : MonoBehaviour
             braking = false;
         }
 
-        if (leftPovorot ) transform.Rotate(transform.up, -control);
-        if (rightPovorot) transform.Rotate(transform.up, control);
-        if (braking && currentSpeed > maxSpeed * 0.4f) currentSpeed -= Time.deltaTime * brake;
+        if (leftPovorot ) transform.Rotate(transform.up, -control_degree);
+        if (rightPovorot) transform.Rotate(transform.up, control_degree);
+        if (braking && currentSpeed > maxSpeed_km_per_h * 0.4f) currentSpeed -= Time.deltaTime * brake_km_per_h2;
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> pageList = new List<GameObject>();
+    [SerializeField] private GameObject levelButtonPrefab;
+    private int page = 0;
+
+    private void Awake()
+    {
+        pageList[0].SetActive(true);
+        CreateLevelsPage();
+    }
+
+    public void SetActivePage(int index)
+    {
+        foreach (var page in pageList)
+        {
+            page.SetActive(false);
+        }
+        pageList[index].SetActive(true);
+    }
+
+    private void CreateLevelsPage()
+    {
+        //i - номер сцены-уровня 
+        for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            float x, y;
+            x = -350+150*(i-1);
+            y = -250;
+
+            GameObject go = Instantiate(levelButtonPrefab);
+            go.transform.parent = pageList[1].transform;
+
+
+            go.GetComponent<RectTransform>().anchoredPosition = new Vector2(x,y);
+
+            var button = go.GetComponent<Button>();
+            go.GetComponentInChildren<Text>().text = i.ToString();
+            button.onClick.AddListener(() => SetLevelForButton(int.Parse(go.GetComponentInChildren<Text>().text)));
+        }
+    }
+
+    private void SetLevelForButton(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
